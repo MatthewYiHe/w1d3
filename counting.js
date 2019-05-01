815842192204cfefd140d3bf27ee5f7502476a8a
MatthewYiHe <matthewhe1@hotmail.com>
2019-05-01 19:58:51 +0000

    completed counting

--- counting.js
+++ counting.js
@@ -1,1 +1,12 @@
-counting.js
+var countLetters = function(input){
+  var noSpace = input.split(" ").join("");
+  var letterCounting = {};
+  for (i = 0; i < noSpace.length; i++){
+    letterCounting[noSpace[i]] = noSpace.split(noSpace[i]).length - 1;
+  }
+  console.log(letterCounting);
+};
+
+
+
+countLetters('lighthouse in the house');
---
+++ test.js
@@ -0,0 +1,15 @@
+function countLetters(str) {
+    var noSpace = str.replace(/\s/g, "");
+    var letterCount = {};
+    for (var i = 0; i < noSpace.length; i++) {
+        var char = noSpace[i]
+        if (letterCount[char]) {
+            letterCount[char] += 1;
+        } else {
+            letterCount[char] = 1
+        }
+    }
+    console.log(letterCount);
+    return letterCount;
+}
+countLetters("lighthouse in the house");
